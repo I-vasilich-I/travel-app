b23b47e903fe4512ba617f34c28d54a925ba32c5
Denis Korotkevich <volk.denis98@gmail.com>
2021-03-07 17:38:26 +0300

    feat: add summary block about country

---
+++ src/modules/ImageSlider.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+function ImageSlider() {
+  const settings = {
+
+  }
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
+  );
+}
+
+export default ImageSlider;
