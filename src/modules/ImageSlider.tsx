2132f7598335a366a09d8fca2b8b760c7922cfbf
Denis Korotkevich <volk.denis98@gmail.com>
2021-03-11 00:16:38 +0300

    feat: add hots keys move slides for images-slider

--- src/modules/ImageSlider.tsx
+++ src/modules/ImageSlider.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Slider from 'react-slick';
 
 import {ImageSlide, ImagesSlideType} from "./ImageSlide";
@@ -9,14 +9,15 @@ interface ImagesSliderProps {
   slides: Array<ImagesSlideType>,
 }
 
-function ImageSlider(props: ImagesSliderProps): JSX.Element {
+export default function ImageSlider(props: ImagesSliderProps): JSX.Element {
 
-  const [fullScreen, setFullScreen] = useState(false);
+  const [fullScreenSlider, setFullScreenSlider] = useState(false);
+  const imageSliderRef = useRef<Slider>(null);
 
   const {slides} = props;
 
-  function fullscreenChange(): void {
-    setFullScreen(!fullScreen);
+  function fullscreenChange(value: boolean): void {
+    setFullScreenSlider(value);
   }
 
   const settings = {
@@ -39,15 +40,33 @@ function ImageSlider(props: ImagesSliderProps): JSX.Element {
     fade: true,
   }
 
+  function changeSlideNext() {
+    if (fullScreenSlider) {
+      imageSliderRef.current?.slickNext();
+    }
+  }
+
+  function changeSlidePrev() {
+    if (fullScreenSlider) {
+      imageSliderRef.current?.slickPrev();
+    }
+  }
+
+  useKey('ArrowRight', changeSlideNext);
+  useKey('ArrowLeft', changeSlidePrev);
   return (
-    <div className={`slider-box ${fullScreen ? 'slider-box__fullscreen' : ''}`}>
-      <button className='btn-fullscreen' title='Закрыть режим просмотра'><CloseIcon/></button>
-      <Slider  {...settings}>
+    <div className={`slider-box ${fullScreenSlider ? 'slider-box__fullscreen' : ''}`}>
+      <button className='btn-fullscreen' title='Закрыть режим просмотра' onClick={() => {
+        fullscreenChange(false)
+      }}><CloseIcon/></button>
+      <Slider ref={imageSliderRef}  {...settings}>
         {
           slides.map((slide, index) => {
             return (
               <ImageSlide key={index} image={slide.image} title={slide.title} description={slide.description}
-                          location={slide.location} onSlideClick={fullscreenChange}/>
+                          location={slide.location} onSlideClick={() => {
+                fullscreenChange(true)
+              }}/>
             )
           })
         }
@@ -56,4 +75,22 @@ function ImageSlider(props: ImagesSliderProps): JSX.Element {
   );
 }
 
-export default ImageSlider;
+function useKey(code: string, callback: (event: KeyboardEvent) => void) {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  useEffect(() => {
+
+    function handle(event: KeyboardEvent) {
+      if (event.code === code) {
+        callbackRef.current(event);
+      }
+    }
+
+    document.addEventListener('keydown', handle);
+    return () => document.removeEventListener('keydown', handle);
+  }, [code]);
+}
