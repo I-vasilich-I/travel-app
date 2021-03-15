3df97a498976dcf9920938ecd7d0cd2fc8e9de72
Denis Korotkevich <volk.denis98@gmail.com>
2021-03-16 00:58:40 +0300

    fix: fix displayed map

--- src/modules/countrypage/Map.tsx
+++ src/modules/countrypage/Map.tsx
@@ -1,20 +1,63 @@
 import React from 'react';
-import {MapContainer, Popup, Marker, TileLayer} from "react-leaflet";
+import {MapContainer, Marker, TileLayer, GeoJSON, Tooltip} from "react-leaflet";
+import {LatLngExpression} from 'leaflet';
 
-export default function Map():JSX.Element {
+import 'leaflet/dist/images/marker-shadow.png';
+
+
+interface MapProps {
+  location: LatLngExpression,
+  lang: string,
+  coordinates: Array<LatLngExpression[]>,
+  country: string,
+  capital: string
+}
+
+export default function Map(props: MapProps): JSX.Element {
+
+  const {location, lang, coordinates, capital, country} = props;
+
+  const GeoJsonData = {
+    geometry: {
+      coordinates: coordinates,
+      type: 'Polygon' as "Polygon",
+    },
+    properties: {
+      name: country
+    },
+    type: 'Feature' as "Feature",
+  }
+
+  const stylesBorder = {
+    color: '#30BD1D',
+    weight: 4
+  }
+
+  function selectLang(lang:string):string {
+    switch (lang) {
+      case 'ru': return 'Столица';
+      case 'en': return 'Capital';
+      case 'de': return 'Hauptstadt';
+      default: return 'Столица';
+    }
+  }
+
   return (
     <div className='map-wrapper'>
-    <MapContainer className='map-content' center={[51.505, -0.09]} zoom={1}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+      <MapContainer className='map-content' center={location} minZoom={2} zoom={5}
+                    maxBounds={[[-89.98155760646617, -180], [89.99346179538875, 180]]} maxBoundsViscosity={1}>
+        <TileLayer
+          updateInterval={20}
+          attribution='<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors'
+          url={`https://tile.jawg.io/jawg-streets/{z}/{x}/{y}.png?lang=${lang}&access-token=hjwgVWeFpBZKCpiJuubyxLQTlfaKby0nFconlrJRUxRilidPFenqyUvzELqTeEjR`}
+        />
+        <Marker position={location}>
+          <Tooltip className='tooltip'>{`${selectLang(lang)}:${capital}`}</Tooltip>
+        </Marker>
+        <GeoJSON data={GeoJsonData} style={stylesBorder}>
+          <Tooltip className='tooltip'>{country}</Tooltip>
+        </GeoJSON>
+      </MapContainer>
     </div>
   );
 }
